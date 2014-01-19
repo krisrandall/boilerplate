c959a2ddcd83f3d10b9710cf8e44768f15d10b7d
kris <kris.randall@gmail.com>
2014-01-19 23:58:24 +1000

    add hamburger menu for aside

--- libs/boilerplate/js/boilerplate.js
+++ libs/boilerplate/js/boilerplate.js
@@ -3,16 +3,41 @@
 
 
 var boilerplate = function() {
-	
-	
-	
-	
-	
+
 }();
 
 
 
 $(document).ready(function() {
+	
+	// add in hamburger menu to open aside if aside is present
+	if ($('#aside').length>0) {
+		var hamburger = '<img id="hamburger" src="libs/icons/hamburger.png"></img>';
+		$('#header').append(hamburger);	
+		$('#hamburger').on('click', function() {
+			
+			if (!$('#aside').is(':visible')) {
+				$('#aside').show();
+				$('#header, #footer, #main').transition({ 'left': $('#aside').width() }, 300);
+			} else {
+				$('#header, #footer, #main').transition({ 'left': '0' }, 300, function() {
+					$('#aside').hide(); // in case there is no BG color on the main area's
+				});
+			}	
+					
+			/* 
+			if ($('#aside').is(':visible')) {
+				$('#aside').animate({ 'left': '-'+$('#aside').width() }, 600);
+			} else {
+				alert('-'+$('#aside').width()+'px');
+				$('#aside').css({ 'left': '-'+$('#aside').width()+'px' })
+						   .show()
+						   .animate({ 'left': '0' }, 600);
+			}
+			*/
+			
+		});
+	}
 	
 	
 });
